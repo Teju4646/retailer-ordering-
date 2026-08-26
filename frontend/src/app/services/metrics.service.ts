687aec5b7b5788fd269ff147ed0bf0b170f28261
Tejaswi Koppula <tejaswik4646@gmail.com>
2026-08-26 16:07:27 -0400

    Add MetricsService for fetching metrics summary

---
+++ frontend/src/app/services/metrics.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class MetricsService {
+  constructor(private http: HttpClient) {}
+
+  getSummary() {
+    return this.http.get<any>(`${environment.apiUrl}/metrics/summary`);
+  }
+}
